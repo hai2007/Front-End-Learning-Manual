adf9b4cc8d7bda2d4cb42220e365f797e5280611
你好2007 <2501482523@qq.com>
2021-11-23 14:39:25 +0800

    ArrayBuffer / TypedArray / DataView

--- src/App/pages/lazy-load.ts
+++ src/App/pages/lazy-load.ts
@@ -1,6 +1,5 @@
 export default {
     "HTML": () => import('./HTML/index'),
     "CSS": () => import('./CSS/index'),
-    "JavaScript": () => import('./JavaScript/index'),
-    "Vue2.js": () => import('./Vue2.js/index')
+    "JavaScript": () => import('./JavaScript/index')
 }

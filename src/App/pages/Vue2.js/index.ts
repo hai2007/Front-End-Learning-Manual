305c1773a43efa71a762fab5c933611d0019cfb8
你好2007 <2501482523@qq.com>
2021-11-23 11:51:59 +0800

    CSS选择器

---
+++ src/App/pages/Vue2.js/index.ts
@@ -0,0 +1,12 @@
+import { Component } from 'nefbl'
+
+import template from './index.html'
+
+@Component({
+    selector: "Vue2.js",
+    template,
+    styles: []
+})
+export default class {
+
+}
--- src/App/pages/lazy-load.ts
+++ src/App/pages/lazy-load.ts
@@ -1,5 +1,6 @@
 export default {
     "HTML": () => import('./HTML/index'),
     "CSS": () => import('./CSS/index'),
-    "JavaScript": () => import('./JavaScript/index')
+    "JavaScript": () => import('./JavaScript/index'),
+    "Vue2.js": () => import('./Vue2.js/index')
 }
